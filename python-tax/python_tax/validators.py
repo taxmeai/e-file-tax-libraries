8265a41470749b9d30a335e7f923a9bdb3c2c8fc
Ope Olatunji <114863803+ope-olatunji@users.noreply.github.com>
2025-06-08 22:02:51 -0400

    Create validators.py

---
+++ python-tax/python_tax/validators.py
@@ -0,0 +1,19 @@
+"""
+Validation utilities for tax data
+"""
+
+class TaxDataValidator:
+    @staticmethod
+    def validate_ssn(ssn):
+        # Implementation here
+        return True
+
+class FormValidator:
+    @staticmethod
+    def validate(form_data):
+        return {'valid': True, 'errors': []}
+
+class CalculationValidator:
+    @staticmethod
+    def validate_calculation(result):
+        return True
